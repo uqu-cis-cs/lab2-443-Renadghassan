f32bd8ae44043dce6f77473b05ae82f6fc359d52
Renadghassan <127680585+Renadghassan@users.noreply.github.com>
2023-04-09 07:03:29 +0000

    Done

--- src/main/java/edu/uqu/cs/Home.java
+++ src/main/java/edu/uqu/cs/Home.java
@@ -14,39 +14,39 @@ public class Home{
 
 		// Create an object of type Garage 
                 //HERE
-                Garage ThGarage = new Garage();
+                Garage TheGarage = new Garage();
 
 
 	        // Add "Toyota Corolla Cross" in the garage. Hint: use method addCar () of class Garage
                 //HERE
-                ThGarage.addCar("Toyota Corolla Cross");
+                TheGarage.addCar("Toyota Corolla Cross");
 
                 //List all the cars in the garage. Hint: use method listCars () of class Garage
                 //HERE
                 System.out.println("All cars in the garage are:");
-                ThGarage.listCars();
+                TheGarage.listCars();
         
                 // Place "Ford Fusion" in the garage. Hint: use method addCar () of class Garage
                 //HERE
-                ThGarage.addCar("Ford Fusion");
+                TheGarage.addCar("Ford Fusion");
 
  	        // List all the cars in the garage once again. Hint: use method listCars () of class Garage
                 //HERE
                 System.out.println("");
                 System.out.println("All cars in the garage are:");
-                 ThGarage.listCars();
+                 TheGarage.listCars();
      
 
                // Move out "Ford Fusion" from the garage. Hint: use method moveOut () of class Garage
                //HERE
-               ThGarage.moveOut("Ford Fusion");
+               TheGarage.moveOut("Ford Fusion");
 
 
                // List all the cars in the garage. Hint: use method listCars () of class Garage
                //HERE
                System.out.println("");
                 System.out.println("All cars in the garage are:");
-                ThGarage.listCars();
+                TheGarage.listCars();
         
         
         }
